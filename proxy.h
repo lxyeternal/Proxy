bcc6341b5f1ca1559cb3535c5ef0b1cceb3d2670
wenboi <1796369212@qq.com>
2018-12-19 18:42:40 +0800

    使用gtk实现的UI界面

---
+++ proxy.h
@@ -0,0 +1,445 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<gtk/gtk.h>
+#include <netdb.h>   
+#include <sys/socket.h>  
+#include "csapp.h"
+
+
+#define MAX_MALLOC 10
+char *ip[100] = {0};
+char *www[100] = {0};
+
+static const char *user_agent_str = "User-Agent: Mozilla/5.0 \
+(X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *accept_str = "Accept: text/html,application/xhtml+xml,\
+application/xml;q=0.9,*/*;q=0.8\r\n";
+static const char *accept_encoding_str = "Accept-Encoding: \
+gzip, deflate\r\n";
+static const char *connection_str = "Connection: close\r\n";
+static const char *proxy_connection_str = "Proxy-Connection: close\r\n";
+static const char *http_version_str = "HTTP/1.0\r\n";
+/* client_bad_request_str used when host is invalid */
+static const char *client_bad_request_str = "HTTP/1.1 400 \
+Bad Request\r\nServer: Apache\r\nContent-Length: 140\r\nConnection: \
+close\r\nContent-Type: text/html\r\n\r\n<html><head></head><body><p>\
+This webpage is not available, because DNS lookup failed.</p></body></html>";
+
+
+void job(void *arg);
+int forward_to_server(int fd, int *to_server_fd);
+int forward_to_client(int to_client_fd, int to_server_fd);
+
+/* helper functions */
+int parse_request_line(char *buf, char *method, char *protocol,
+                       char *host_port, char *resource, char *version);
+void parse_host_port(char *host_port, char *remote_host, char *remote_port);
+void get_size(char *buf, unsigned int *size_pointer);
+void close_fd(int *to_client_fd, int *to_server_fd);
+
+
+
+
+// int run (GtkWidget *widget, gpointer *data){
+//     int listenfd, *connfd, clientlen, port = 0;
+//     struct sockaddr_in clientaddr;
+//     pthread_t tid;
+
+//     // ignore SIGPIPE
+//     Signal(SIGPIPE, SIG_IGN);
+
+//     // check input
+
+//     port = 5555;
+//     if (port == 0) {
+//         fprintf(stderr, "please enter valid port number\n");
+//         exit(1);
+//     }
+
+//     listenfd = Open_listenfd(port);
+//     if (listenfd < 0) {
+//         fprintf(stderr, "cannot listen to port: %d\n", port);
+//         exit(1);
+//     }
+
+//     while (1) {
+//         clientlen = sizeof(clientaddr);
+//         int count = 0;
+//         // use MAX_MALLOC to limit max tries
+//         while ((connfd = (int *)malloc(sizeof(int))) == NULL) {
+//             if (count > MAX_MALLOC) {
+//                 break;
+//             }
+//             count++;
+//             sleep(1);
+//         }
+//         *connfd = Accept(listenfd, (SA *)&clientaddr,
+//                          (socklen_t *)&clientlen);
+//         Pthread_create(&tid, NULL, (void *)job, (void *)connfd);
+//     }
+//     return 0;
+// }
+
+
+void job(void *arg) {
+    Pthread_detach(pthread_self());
+    // get file descriptor and free the pointer
+    int to_client_fd = *(int *)arg;
+    Free(arg);
+
+    int to_server_fd = -1;
+    int rc = 0;
+
+    rc = forward_to_server(to_client_fd, &to_server_fd);
+    if (rc == -1) {
+        // some error
+        close_fd(&to_client_fd, &to_server_fd);
+        Pthread_exit(NULL);
+    } else{
+        if (forward_to_client(to_client_fd, to_server_fd) == -1) {
+            close_fd(&to_client_fd, &to_server_fd);
+            Pthread_exit(NULL);
+        }
+    }
+    close_fd(&to_client_fd, &to_server_fd);
+    return;
+}
+
+
+
+int forward_to_server(int fd, int *to_server_fd) {
+    char buf[MAXLINE], request_buf[MAXLINE];
+    char method[MAXLINE], protocol[MAXLINE];
+    char host_port[MAXLINE];
+    char remote_host[MAXLINE], remote_port[MAXLINE], resource[MAXLINE];
+    char version[MAXLINE];
+    char origin_request_line[MAXLINE];
+    char origin_host_header[MAXLINE];
+    char url[MAXLINE]={0};
+    //char *ip[100]={0};
+	//char *www[100]={0};s
+	int i;
+	char **pptr;   
+    struct hostent *hptr;   
+    char str[32];   
+    char dest_ip[32];
+    int flag=1;
+    
+    int has_user_agent_str = 0, has_accept_str = 0,
+        has_accept_encoding_str = 0,
+        has_connection_str = 0, has_proxy_connection_str = 0,
+        has_host_str = 0;
+
+    rio_t rio_client;
+
+    strcpy(remote_host, "");
+    strcpy(remote_port, "80");
+
+    Rio_readinitb(&rio_client, fd);
+    if (Rio_readlineb(&rio_client, buf, MAXLINE) == -1) {
+        return -1;
+    }
+    // used incase dns lookup failed
+    strcpy(origin_request_line, buf);
+
+    if (parse_request_line(buf, method, protocol, host_port,
+                           resource, version) == -1) {
+        return -1;
+    }
+
+	strcpy(url,protocol);
+	strcat(url,"://");
+	strcat(url,host_port);
+	strcat(url,resource);
+	printf("*********%s\n",url);
+	
+	for(i=0;i<100;i++)
+	{
+		if(!www[i])
+		{
+			break;
+		}
+		if(strstr(url,www[i]))
+		{ 
+			return -1;
+		}
+	}
+	
+	
+    parse_host_port(host_port, remote_host, remote_port);
+
+
+
+    if ((hptr = gethostbyname(remote_host)) == NULL)   
+    {   
+        printf("error\n");   
+        return -1;
+    }  
+    
+	pptr=hptr->h_addr_list;
+    strcpy(dest_ip,(char*)(inet_ntop(hptr->h_addrtype, *pptr, str, sizeof(str))));
+    printf(" address:%s\n", dest_ip);
+    
+    for(i=0;i<100;i++)
+	{
+		if(!ip[i])
+		{
+			break;
+		}
+		if(strstr(dest_ip,ip[i]))
+		{
+			return -1;
+		}
+	}
+	
+
+    if (strstr(method, "GET") != NULL) {
+        // GET method
+
+        // compose our request line
+        strcpy(request_buf, method);
+        strcat(request_buf, " ");
+        strcat(request_buf, resource);
+        strcat(request_buf, " ");
+        strcat(request_buf, http_version_str);
+
+        // process request header
+        while (Rio_readlineb(&rio_client, buf, MAXLINE) != 0) {
+            if (strcmp(buf, "\r\n") == 0) {
+                break;
+            } else if (strstr(buf, "User-Agent:") != NULL) {
+                strcat(request_buf, user_agent_str);
+                has_user_agent_str = 1;
+            } else if (strstr(buf, "Accept-Encoding:") != NULL) {
+                strcat(request_buf, accept_encoding_str);
+                has_accept_encoding_str = 1;
+            } else if (strstr(buf, "Accept:") != NULL) {
+                strcat(request_buf, accept_str);
+                has_accept_str = 1;
+            } else if (strstr(buf, "Connection:") != NULL) {
+                strcat(request_buf, connection_str);
+                has_connection_str = 1;
+            } else if (strstr(buf, "Proxy Connection:") != NULL) {
+                strcat(request_buf, proxy_connection_str);
+                has_proxy_connection_str = 1;
+            } else if (strstr(buf, "Host:") != NULL) {
+                strcpy(origin_host_header, buf);
+                if (strlen(remote_host) < 1) {
+                    sscanf(buf, "Host: %s", host_port);
+                    parse_host_port(host_port, remote_host, remote_port);
+                }
+                strcat(request_buf, buf);
+                has_host_str = 1;
+            } else {
+                strcat(request_buf, buf);
+            }
+        }
+        // if not sent, copy in out headers
+        if (has_user_agent_str != 1) {
+            strcat(request_buf, user_agent_str);
+        }
+        if (has_accept_encoding_str != 1) {
+            strcat(request_buf, accept_encoding_str);
+        }
+        if (has_accept_str != 1) {
+            strcat(request_buf, accept_str);
+        }
+        if (has_connection_str != 1) {
+            strcat(request_buf, connection_str);
+        }
+        if (has_proxy_connection_str != 1) {
+            strcat(request_buf, proxy_connection_str);
+        }
+        if (has_host_str != 1) {
+            sprintf(buf, "Host: %s:%s\r\n", remote_host, remote_port);
+            strcat(request_buf, buf);
+        }
+        strcat(request_buf, "\r\n");
+        if (strcmp(remote_host, "") == 0) {
+            return -1;
+        }
+
+
+        // client to server
+        *to_server_fd = Open_clientfd(remote_host, atoi(remote_port),
+                                    origin_request_line, origin_host_header);
+        if (*to_server_fd == -1) {
+            return -1;
+        } else if (*to_server_fd == -2) {
+            // dns lookup failed, write our response page
+            // caused by invalid host
+            strcpy(buf, client_bad_request_str);
+            Rio_writen(fd, buf, strlen(buf));
+            return -1;
+        }
+        if (Rio_writen(*to_server_fd, request_buf,
+                       strlen(request_buf)) == -1) {
+            return -1;
+        }
+        return 0;
+    } else {
+        // non GET method
+        unsigned int length = 0, size = 0;
+        strcpy(request_buf, buf);
+        while (strcmp(buf, "\r\n") != 0 && strlen(buf) > 0) {
+            if (Rio_readlineb(&rio_client, buf, MAXLINE) == -1) {
+                return -1;
+            }
+            if (strstr(buf, "Host:") != NULL) {
+                strcpy(origin_host_header, buf);
+                if (strlen(remote_host) < 1) {
+                    sscanf(buf, "Host: %s", host_port);
+                    parse_host_port(host_port, remote_host, remote_port);
+                }
+            }
+            get_size(buf, &size);
+            strcat(request_buf, buf);
+        }
+        if (strcmp(remote_host, "") == 0) {
+            return -1;
+        }
+        *to_server_fd = Open_clientfd(remote_host, atoi(remote_port),
+                                    origin_request_line, origin_host_header);
+        if (*to_server_fd < 0) {
+            return -1;
+        }
+        // write request line
+        if (Rio_writen(*to_server_fd, request_buf,
+                       strlen(request_buf)) == -1) {
+            return -1;
+        }
+        // write request body
+        while (size > MAXLINE) {
+            if ((length = Rio_readnb(&rio_client, buf, MAXLINE)) == -1) {
+                return -1;
+            }
+            if (Rio_writen(*to_server_fd, buf, length) == -1) {
+                return -1;
+            }
+            size -= MAXLINE;
+        }
+        if (size > 0) {
+            if ((length = Rio_readnb(&rio_client, buf, size)) == -1) {
+                return -1;
+            }
+            if (Rio_writen(*to_server_fd, buf, length) == -1) {
+                return -1;
+            }
+        }
+        return 2;
+    }
+}
+
+
+int forward_to_client(int to_client_fd, int to_server_fd) {
+    rio_t rio_server;
+    char buf[MAXLINE];
+    unsigned int length = 0, size = 0;
+
+    Rio_readinitb(&rio_server, to_server_fd);
+    // forward status line
+    if (Rio_readlineb(&rio_server, buf, MAXLINE) == -1) {
+        return -1;
+    }
+    if (Rio_writen(to_client_fd, buf, strlen(buf)) == -1) {
+        return -1;
+    }
+    // forward response headers
+    while (strcmp(buf, "\r\n") != 0 && strlen(buf) > 0) {
+        if (Rio_readlineb(&rio_server, buf, MAXLINE) == -1) {
+            return -1;
+        }
+        get_size(buf, &size);
+        if (Rio_writen(to_client_fd, buf, strlen(buf)) == -1) {
+            return -1;
+        }
+    }
+    // forward response body
+    if (size > 0) {
+        while (size > MAXLINE) {
+            if ((length = rio_readnb(&rio_server, buf, MAXLINE)) == -1) {
+                return -1;
+            }
+            if (Rio_writen(to_client_fd, buf, length) == -1) {
+                return -1;
+            }
+            size -= MAXLINE;
+        }
+        if (size > 0) {
+            if ((length = Rio_readnb(&rio_server, buf, size)) == -1) {
+                return -1;
+            }
+            if (Rio_writen(to_client_fd, buf, length) == -1) {
+                return -1;
+            }
+        }
+    } else {
+        while ((length = Rio_readnb(&rio_server, buf, MAXLINE)) > 0) {
+            if (Rio_writen(to_client_fd, buf, length) == -1) {
+                return -1;
+            }
+        }
+    }
+    return 0;
+}
+
+
+
+int parse_request_line(char *buf, char *method, char *protocol,
+                       char *host_port, char *resource, char *version) {
+    char url[MAXLINE];
+    // check if it is valid buffer
+    if (strstr(buf, "/") == NULL || strlen(buf) < 1) {
+        return -1;
+    }
+    // set resource default to '/'
+    strcpy(resource, "/");
+    sscanf(buf, "%s %s %s", method, url, version);
+    if (strstr(url, "://") != NULL) {
+        // has protocol
+        sscanf(url, "%[^:]://%[^/]%s", protocol, host_port, resource);
+    } else {
+        // no protocols
+        sscanf(url, "%[^/]%s", host_port, resource);
+    }
+    return 0;
+}
+
+
+
+
+/*
+ * get_size - get size from content-length header
+ */
+void get_size(char *buf, unsigned int *size_pointer) {
+    if (strstr(buf, "Content-Length")) {
+        sscanf(buf, "Content-Length: %d", size_pointer);
+    }
+}
+
+
+/*
+ * parse_host_port - parse host:port (:port optional)to two parts
+ */
+void parse_host_port(char *host_port, char *remote_host, char *remote_port) {
+    char *tmp = NULL;
+    tmp = index(host_port, ':');
+    if (tmp != NULL) {
+        *tmp = '\0';
+        strcpy(remote_port, tmp + 1);
+    } else {
+        strcpy(remote_port, "80");
+    }
+    strcpy(remote_host, host_port);
+}
+
+
+void close_fd(int *to_client_fd, int *to_server_fd) {
+    if (*to_client_fd >= 0) {
+        Close(*to_client_fd);
+    }
+    if (*to_server_fd >= 0) {
+        Close(*to_server_fd);
+    }
+}
---
+++ ui.c
@@ -0,0 +1,310 @@
+#include<gtk/gtk.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <pthread.h>
+#include "proxy.h"
+
+  char *ip1[100]={0};
+  char *www1[100]={0};
+  char entrystring[200];
+
+struct _WWW{
+    char *ip[100];
+    char *www[100];
+    int countip;
+    int countwww;
+};
+
+struct _WWW WWW = {.countip = 0,.countwww = 0};
+
+
+int button_run(void *arg);
+
+
+GtkWidget *window_test;
+
+
+void threads(GtkWidget *widget,gpointer *data)
+{
+  pthread_t thread1;
+  int ret_thrd1;
+  ret_thrd1 = pthread_create(&thread1, NULL, (void *)&button_run, (void *)&WWW);
+  pthread_join(thread1,NULL);
+}
+
+void enter_callback( GtkWidget *widget, gpointer entry ) 
+{ 
+  const gchar *entry_text; 
+  char test[100];
+  char *filterstring[100] = {0};
+  char spl[] = " ";
+  char *result = NULL;
+  int i=0,j;
+  int countip=0;
+  int countwww=0;
+
+	// 获得文本内容
+  entry_text = gtk_entry_get_text(GTK_ENTRY(entry)); 
+  strcpy(entrystring, entry_text);
+  filterstring[0] = strtok(entrystring,spl);
+  while(filterstring[i])
+  {
+    i++;
+    filterstring[i] = strtok(NULL,spl);
+  }
+  j = i;
+  for(i = 0;i<j;i++)
+  {
+    strcpy(test,filterstring[i]);
+      if(test[0]>='1'&&test[0]<='9')
+      {
+          ip1[countip] = filterstring[i];
+          countip++;
+      }
+      else
+      {
+        www1[countwww] = filterstring[i];
+          countwww++;
+      }
+  }
+
+  WWW.countip = countip;
+  WWW.countwww = countwww;
+  
+  for(int k=0;k<countip;k++)
+  {
+      WWW.ip[k] = ip1[k];
+    //   printf("%s\n",WWW.ip[k]);
+  }
+
+  for(int k=0;k<countwww;k++)
+  {
+      WWW.www[k] = www1[k];
+    //   printf("%s\n",WWW.www[k]);
+  }
+
+}
+
+int button_run (void *arg){
+
+    int listenfd, *connfd, clientlen, port = 0;
+    struct sockaddr_in clientaddr;
+    pthread_t tid;
+    struct _WWW *pstru;       
+    pstru = ( struct _WWW *) arg;
+    printf("%d\n",pstru->countip);
+    printf("%d\n",pstru->countwww);
+    for(int i=0;i<pstru->countip;i++)
+    {
+        ip[i] = pstru->ip[i];
+    }
+    for(int m=0;m<pstru->countwww;m++)
+    {
+        www[m] = pstru->www[m];
+    }
+
+    for(int m=0;m<pstru->countwww;m++)
+    {
+        printf("%s\n",www[m]);
+    }
+   
+
+    // ignore SIGPIPE
+    Signal(SIGPIPE, SIG_IGN);
+
+    // check input
+
+    port = 10000;
+    if (port == 0) {
+        fprintf(stderr, "please enter valid port number\n");
+        exit(1);
+    }
+
+    listenfd = Open_listenfd(port);
+    if (listenfd < 0) {
+        fprintf(stderr, "cannot listen to port: %d\n", port);
+        exit(1);
+    }
+
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        int count = 0;
+        // use MAX_MALLOC to limit max tries
+        while ((connfd = (int *)malloc(sizeof(int))) == NULL) {
+            if (count > MAX_MALLOC) {
+                break;
+            }
+            count++;
+            sleep(1);
+        }
+        *connfd = Accept(listenfd, (SA *)&clientaddr,
+                         (socklen_t *)&clientlen);
+        Pthread_create(&tid, NULL, (void *)job, (void *)connfd);
+    }
+    return 0;
+}
+
+void button_event(GtkWidget *widget, gpointer *data) /*回调函数*/
+
+{
+  exit(0);
+}
+
+void show_test_window(GtkWidget *widget, gpointer data)
+{
+  gtk_widget_show_all(window_test);
+}
+
+/* 用指定的参数创建一个按钮盒 */
+GtkWidget *create_bbox( gint  horizontal,
+                        gint  spacing,
+                        gint  child_w,
+                        gint  child_h,
+                        gint  layout )
+{
+  GtkWidget *frame;
+  GtkWidget *bbox;
+  GtkWidget *button1;
+  GtkWidget *button2;
+  GtkWidget *button3;
+
+  // frame = gtk_frame_new (title);
+
+  if (horizontal)
+    bbox = gtk_hbutton_box_new ();
+  else
+    bbox = gtk_vbutton_box_new ();
+
+  gtk_container_set_border_width (GTK_CONTAINER (bbox), 5);
+  gtk_container_add (GTK_CONTAINER (frame), bbox);
+
+  /* 设置按钮盒的外观 */
+  gtk_button_box_set_layout (GTK_BUTTON_BOX (bbox), layout);
+  gtk_box_set_spacing (GTK_BOX (bbox), spacing);
+  /*gtk_button_box_set_child_size (GTK_BUTTON_BOX (bbox), child_w, child_h);*/
+
+  button1 = gtk_button_new_with_label("连接");
+  gtk_container_add (GTK_CONTAINER (bbox), button1);
+  
+
+  button2 = gtk_button_new_with_label("退出");
+  gtk_container_add (GTK_CONTAINER (bbox), button2);
+ 
+
+  button3 = gtk_button_new_with_label("帮助");
+  gtk_container_add (GTK_CONTAINER (bbox), button3);
+  // gtk_signal_connect(GTK_OBJECT(button3), "clicked", GTK_SIGNAL_FUNC(button_event), "clicked");
+    gtk_signal_connect(GTK_OBJECT(button1), "clicked", GTK_SIGNAL_FUNC(threads),NULL);
+    gtk_signal_connect(GTK_OBJECT(button2), "clicked", GTK_SIGNAL_FUNC(button_event),NULL);
+    g_signal_connect(G_OBJECT(button3), "clicked", G_CALLBACK(show_test_window),NULL);
+
+  return frame;
+}
+
+GtkWidget *create_test_window()    //点击帮助按钮然后弹出帮助页面
+{
+  GtkWidget *text;
+  GtkWidget *vbox;
+  GtkWidget *image;
+  GtkWidget *image_two;
+  GdkPixbuf *src;
+  GdkPixbuf *dest;
+
+  window_test = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window_test),"帮助");
+
+  vbox = gtk_vbox_new(FALSE, 0);
+  gtk_container_add(GTK_CONTAINER(window_test), vbox);
+  gtk_window_set_default_size(GTK_WINDOW(window_test), 400, 300);
+
+  // image = get_image_new_from_file("1.jpg");
+  // gtk_container_add(GTK_CONTAINER(vbox),image);
+
+  src = gdk_pixbuf_new_from_file("1.jpg",NULL);
+  dest = gdk_pixbuf_scale_simple(src,100,100,GDK_INTERP_BILINEAR);
+  image_two = gtk_image_new_from_pixbuf(dest);
+  g_object_unref(src);
+  g_object_unref(dest);
+  gtk_container_add(GTK_CONTAINER(vbox), image_two);
+
+
+  text = gtk_label_new("  添加URL：直接添加链接或者ip,中间使用空间进行隔离"\
+                       "  过滤多个url的时候，回车换行输入多个\n"
+                       "  编译方式：gcc -g -Wall -o mp csapp.c cache.c proxy.c -lpthread\n"
+                       "  支持平台：在Ubuntu下可以运行\n"
+                       "  协议支持：支持http协议GET、POST方法，带缓存\n");
+  gtk_container_add(GTK_CONTAINER(vbox), text);
+
+
+  return window_test;
+}
+
+int main(int argc, char const *argv[])
+  {
+    static GtkWidget *window = NULL;
+    GtkWidget *main_vbox;
+    GtkWidget *vbox;
+    GtkWidget *hbox;
+    GtkWidget *entry;
+    GtkWidget *label;
+    GtkWidget *frame_horz;
+    GtkWidget *frame_vert;
+    GtkWidget *frame_addurl;
+
+    /* 初始化 */
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Proxy");
+    gtk_window_set_default_size(GTK_WINDOW(window), 600, 500);
+
+    g_signal_connect(G_OBJECT(window), "destroy",
+                     G_CALLBACK(gtk_main_quit),
+                     NULL);
+    g_signal_connect(G_OBJECT(window_test), "destroy", G_CALLBACK(gtk_main_quit),
+                     NULL);
+    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+    gtk_window_set_position(GTK_WINDOW(window),GTK_WIN_POS_CENTER_ALWAYS);
+
+    main_vbox = gtk_vbox_new(FALSE, 0);
+    gtk_container_add(GTK_CONTAINER(window), main_vbox);
+
+    frame_vert = gtk_frame_new("版本信息");
+    gtk_box_pack_start(GTK_BOX(main_vbox), frame_vert, TRUE, TRUE, 10);
+    label = gtk_label_new("开发实践：2018-11-30\n"
+                          "开发人员：501组合\n"
+                          "版本信息：V-1.0.0\n"
+                          "反馈：QQ:123455\n");
+    gtk_container_add(GTK_CONTAINER(frame_vert), label);
+
+    frame_addurl = gtk_frame_new("添加过滤的url");
+    gtk_box_pack_start(GTK_BOX(main_vbox), frame_addurl, TRUE, TRUE, 10);
+    entry = gtk_entry_new();
+    gtk_entry_set_max_length(GTK_ENTRY(entry), 1000);
+    gtk_entry_set_text(GTK_ENTRY(entry), "192.168.135.132");
+    gtk_container_add(GTK_CONTAINER(frame_addurl), entry);
+    /* 如果我们想在用户输入文本时进行响应，可以为activate设置回调函数。
+	 * 当用户在文本输入构件内部按回车键时引发Activate信号；
+	 */
+    g_signal_connect(entry, "activate", G_CALLBACK(enter_callback), entry);
+
+    frame_horz = gtk_frame_new("");
+    gtk_box_pack_start(GTK_BOX(main_vbox), frame_horz, TRUE, TRUE, 10);
+
+    vbox = gtk_vbox_new(FALSE, 0);
+    gtk_container_set_border_width(GTK_CONTAINER(vbox), 10);
+    gtk_container_add(GTK_CONTAINER(frame_horz), vbox);
+
+    gtk_box_pack_start(GTK_BOX(vbox),
+                       create_bbox(TRUE, 40, 85, 20, GTK_BUTTONBOX_SPREAD),
+                       TRUE, TRUE, 0);
+
+    gtk_widget_show_all(window);
+    window_test = create_test_window();
+
+    /* 进入事件循环 */
+    gtk_main();
+
+    return 0;
+  }
